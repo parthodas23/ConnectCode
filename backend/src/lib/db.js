187a9a8d2b2badf615a0c2b5dc1fb309aacc69bb
Partha Das <parthodasm@gmail.com>
2025-12-03 16:58:32 +0600

    DB and Setup

---
+++ backend/src/lib/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import { ENV } from "./env.js";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(ENV.DB_URL);
+    console.log("DB connected successfully", conn.connection.host);
+  } catch (error) {
+    console.log("Error happens", error);
+  }
+};

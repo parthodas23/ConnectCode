552dc8e55833a16d5a683972796a0d66f05c5cdc
Partha Das <parthodasm@gmail.com>
2025-12-03 19:12:18 +0600

    sync clerk to db

---
+++ backend/src/models/User.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    profileImage: { type: String, default: "" },
+    clerkId: { type: String, required: true, unique: true },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,15 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
 import path from "path";
+import cors from "cors";
 import { connectDB } from "./lib/db.js";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./lib/inngest.js";
 const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const __dirname = path.resolve();
 

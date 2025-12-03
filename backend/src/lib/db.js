af544fd7a50c86f6230cdc0c8f863241051caab2
Partha Das <parthodasm@gmail.com>
2025-12-03 17:03:07 +0600

    Check if DB url exist or not

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,6 +3,9 @@ import { ENV } from "./env.js";
 
 export const connectDB = async () => {
   try {
+    if (!ENV.DB_URL) {
+      throw new Error("DB url doesn't exist in the environment variable.");
+    }
     const conn = await mongoose.connect(ENV.DB_URL);
     console.log("DB connected successfully", conn.connection.host);
   } catch (error) {
